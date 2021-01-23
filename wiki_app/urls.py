34f2b0784c1a0f45b148dab684f57db7b8b74e2e
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-01-23 13:18:28 +0900

    created models for User and Deity

--- wiki_app/urls.py
+++ wiki_app/urls.py
@@ -3,6 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('back', views.back),
+    path('log_in', views.log_in_page_render),
     path('home_page', views.home_page_render),
-    path('register_page', views.register_page_render)
+    path('register_page', views.register_page_render),
+    path('user_profile_page/<int:user_id>', views.user_profile_page),
+    path('top_ten_page', views.top_ten_page),
+    path('add_entry_page', views.add_entry_page),
+    path('dieties_by_religion', views.deities_by_religion_page),
+    path('dieties_by_location', views.deities_by_location_page),
+    path('diety_info_page/<int:diety_id>', views.deity_info_page),
+    path('register', views.register),
+    path('logout', views.logout),
+    path('login', views.login),
 ]
--- wiki_app/views.py
+++ wiki_app/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import User, Deity
 
 #start render functions
 def index(request):
     return render(request, "welcome_page.html")
+
+def log_in_page_render(request):
+    return render(request, "log_in_page.html")
 
 def home_page_render(request):
     return render(request, "home_page.html")
@@ -17,7 +22,7 @@ def deities_by_religion_page(request):
     return render(request, "deities_by_religion_page.html")
 
 def top_ten_page(request):
-    return render(request, "top_ten-page.html")
+    return render(request, "top_ten_page.html")
 
 def user_profile_page(request, user_id):
     return render(request, "user_profile_page.html")
@@ -31,5 +36,33 @@ def deity_info_page(request, deity_id):
     # doublecheck this one 
 
 #end render pages
+def back(request):
+    return redirect("welcome_page.html")
 
+def register(request):
+    if request.method == "GET":
+        return redirect('register_page')
+    errors = User.objects.validate(request.POST)
+    if errors:
+        for e in errors.values():
+            messages.error(request, e)
+        return redirect('register_page')
+    else:
+        new_user = User.objects.register(request.POST)
+        request.session['user_id'] = new_user.id
+        return redirect('/home_page')
 
+def login(request):
+    if request.method == "GET":
+        return redirect('register_page')
+    if not User.objects.authenticate(request.POST['email'], request.POST['password']):
+        messages.error(request, 'Invalid Email/Password')
+        return redirect('register_page')
+    user = User.objects.get(email=request.POST['email'])
+    request.session['user_id'] = user.id
+    messages.success(request, "You have successfully logged in!")
+    return redirect('home_page')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
